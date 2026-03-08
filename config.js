ed39d053e13ea2a7f42cb078a166b21e3aa38064
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2026-03-08 13:57:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-const API_URL="https://farmotor-s-transport-en-dev.onrender.com";
-const DISCORD_CLIENT_ID="1479893449980514464";
-const DISCORD_REDIRECT_URI="REMPLACE_PAR_TON_URL_DE_CALLBACK";
-const DISCORD_OAUTH_URL=`https://discord.com/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(DISCORD_REDIRECT_URI)}&scope=identify`;
+const API_URL = "https://farmotor-s-transport-en-dev.onrender.com";
+const DISCORD_CLIENT_ID = "1479893449980514464";
+const DISCORD_REDIRECT_URI = "https://routier87.github.io/FarmOtor-s-Transport-en-DEV/";
+const DISCORD_OAUTH_URL =
+"https://discord.com/oauth2/authorize?client_id=1479893449980514464&response_type=code&redirect_uri=https%3A%2F%2Froutier87.github.io%2FFarmOtor-s-Transport-en-DEV%2F&scope=identify+guilds+guilds.members.read";
